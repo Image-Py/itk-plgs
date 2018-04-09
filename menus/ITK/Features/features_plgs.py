d7dea7d64dd11f5923127df2b30b5a712f8aabdf
yxdragon <imagepy@sina.com>
2018-04-09 15:13:11 +0800

    slider

--- menus/ITK/Features/features_plgs.py
+++ menus/ITK/Features/features_plgs.py
@@ -8,8 +8,8 @@ class CannyMagnitude(Filter):
 	note = ['all', 'auto_msk', 'auto_snap', '2float', 'preview']
 	para = {'sigma':1.0, 'low_threshold':10, 'high_threshold':20}
 	view = [(float, (0,10), 1,  'sigma', 'sigma', 'pix'),
-			('slide',(0,50), 'low_threshold', 'low_threshold',''),
-			('slide',(0,50), 'high_threshold', 'high_threshold','')]
+			('slide',(0,50), 4, 'low_threshold', 'low_threshold'),
+			('slide',(0,50), 4, 'high_threshold', 'high_threshold')]
 
 	def run(self, ips, snap, img, para = None):
 		img = sitk.GetImageFromArray(snap)
